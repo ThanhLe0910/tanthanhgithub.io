126ac6e88decc0c2baa2b99d340758c3cdd7619b
tanthanh <letanthanh@lab.frog.company>
2021-03-16 10:36:05 +0700

    update code to php laraver 16/03

---
+++ resources/views/trang.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smart Card</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/css/style.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="abc">
+        <div class="circle">
+            <img src="https://png.pngtree.com/png-clipart/20190516/original/pngtree-cute-girl-avatar-material-png-image_4023832.jpg"
+                width="175px">
+        </div>
+        <div class="textname">
+            <h3 class="text-center mt-3" style="word-spacing: 5px;">Nguyễn Thị Trang</h3>
+        </div>
+        <div class="description text-center">
+            <i>Sự miêu tả</i>
+        </div>
+        <div class="text-center">
+            <i class="fa fa-envelope"> nguyenthitrang0415@gmail.com</i>
+        </div>
+        <div class="text-center">
+            <i class="fa fa-phone" aria-hidden="true"> 0383820415</i>
+        </div>
+        <div class="text-center facebook mt-4" style="border: 1px solid #F5F5DC;box-shadow: 2px 2px 2px wheat;;margin-left: 20px;margin-right: 20px;
+        background-color: #F5F5DC;">
+            <a href="https://www.facebook.com/profile.php?id=100007887192416">
+                <div class="row" style="padding: 10px;">
+                    <div class="col-4">
+                        <div>
+                            <i style="font-size: 30px;" class="fa fa-facebook-official" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <label style="color: black;">Facebook</label>
+                    </div>
+                    <div class="col-4">
+                        <i style="color: black;" class="fa fa-angle-right" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="text-center facebook mt-4" style="border: 1px solid #F5F5DC;box-shadow: 2px 2px 2px wheat;;margin-left: 20px;margin-right: 20px;
+        background-color: #F5F5DC;">
+            <a href="https://www.instagram.com">
+                <div class="row" style="padding: 10px;">
+                    <div class="col-4">
+                        <div>
+                            <img style="background-color:#F5F5DC ;"
+                                src="/image/Capture.PNG" width="30px" height="30px">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <label style="color: black;">Instagram</label>
+                    </div>
+                    <div class="col-4">
+                        <i style="color: black;" class="fa fa-angle-right" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="text-center facebook mt-4" style="border: 1px solid #F5F5DC;box-shadow: 2px 2px 2px wheat;;margin-left: 20px;margin-right: 20px;
+        background-color: #F5F5DC;">
+            <a href="https://chat.zalo.me/">
+                <div class="row" style="padding: 10px;">
+                    <div class="col-4">
+                        <div>
+                            <img style="background-color:#F5F5DC ;"
+                                src="/image/zalo.PNG" width="30px" height="30px">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <label style="color: black;">Zalo</label>
+                    </div>
+                    <div class="col-4">
+                        <i style="color: black;" class="fa fa-angle-right" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="text-center facebook mt-4" style="border: 1px solid #F5F5DC;box-shadow: 2px 2px 2px wheat;;margin-left: 20px;margin-right: 20px;
+        background-color: #F5F5DC;">
+            <a href="https://web.skype.com/">
+                <div class="row" style="padding: 10px;">
+                    <div class="col-4">
+                        <div>
+                            <img style="background-color:#F5F5DC ;"
+                                src="/image/sky.PNG" width="30px" height="30px">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <label style="color: black;">Skype</label>
+                    </div>
+                    <div class="col-4">
+                        <i style="color: black;" class="fa fa-angle-right" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="text-center facebook" style="border: 1px solid #F5F5DC;box-shadow: 2px 2px 2px wheat;
+        background-color: #F5F5DC;margin-top: 29%; font-weight: bold;padding: 20px;">
+            Smart Card
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
